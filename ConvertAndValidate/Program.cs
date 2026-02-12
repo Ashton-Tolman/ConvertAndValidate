f86b802cdba47ad8f10e2386f62e778a50262a3d
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-02-12 08:53:34 -0700

    stuff broke

--- ConvertAndValidate/Program.cs
+++ ConvertAndValidate/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ConvertAndValidate
+﻿/* Ashton Tolman
+ * Spring 2026
+ * ConvertAndValidate
+ * https://github.com/Ashton-Tolman/ConvertAndValidate.git
+ */
+namespace ConvertAndValidate
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = "";
+            bool result = false;
+            ConvertAndValidate(out userInput,result);
+            
+
+            //pause
+            Console.Read();
+        }
+
+        static bool ConvertAndValidate(out string userInputA, bool resultA)
+        {
+            bool result = false;
+            string userInput = "";
+            try
+            {
+                int.Parse(userInput);
+                
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
         }
     }
 }
