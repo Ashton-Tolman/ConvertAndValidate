c8725f9951988271c9264d2624bc5222f828f006
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-02-13 08:27:47 -0700

    Finshed project

--- ConvertAndValidate/Program.cs
+++ ConvertAndValidate/Program.cs
@@ -3,37 +3,55 @@
  * ConvertAndValidate
  * https://github.com/Ashton-Tolman/ConvertAndValidate.git
  */
+using System.Linq.Expressions;
+
 namespace ConvertAndValidate
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string userInput = "";
-            bool result = false;
-            ConvertAndValidate(out userInput,result);
+            int total = 0;
+            Console.WriteLine(ConvertAndValidate(ref total, ""));
             
 
             //pause
             Console.Read();
         }
-
-        static bool ConvertAndValidate(out string userInputA, bool resultA)
+        static bool ConvertAndValidate(ref int total, string userInput)
         {
-            bool result = false;
-            string userInput = "";
-            try
+            bool isNumber;
+            
+            do
             {
-                int.Parse(userInput);
-                
-                result = true;
-            }
-            catch (Exception ex)
-            {
-                result = false;
-            }
-
-            return result;
+                try
+                {
+                    Console.WriteLine("Check for a number:");
+                    userInput = Console.ReadLine();
+                    int.Parse(userInput);
+                    Console.WriteLine("Correct conversion");
+                    isNumber = true;
+                    total += int.Parse(userInput);
+                }
+                catch (Exception)
+                {
+                    if (userInput is not "q" and not "Q")
+                    {
+                        Console.WriteLine("Converstion failed");
+                        isNumber = false;
+                        total = total;
+                    }
+                    else
+                    {
+                        isNumber = false;
+                        total = total;
+                        Console.WriteLine("Goodbye");
+                    }
+                }
+            } while (userInput is not "q" and not "Q");
+            Console.WriteLine($"The total of your numbers is: {total}");
+            Console.WriteLine("last conversion status:");
+            return isNumber;
         }
     }
 }
